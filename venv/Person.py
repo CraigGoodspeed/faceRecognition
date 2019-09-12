d4a496d900b0a93c153b4bec4ef341aee06599e1
craig <craig@goodspeed.co.za>
2019-09-12 22:15:56 +0200

    threaded the sound, performance is better.

---
+++ venv/Person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, image, sound, display_name):
+        self.image = image
+        self.sound = sound
+        self.display_name = display_name
+        self.playing = 0
+
